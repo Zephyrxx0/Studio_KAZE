47e9ad4ad5b103a76d94acccfa12745320752f8f
AyushM <84081476+Zephyrxx0@users.noreply.github.com>
2024-08-08 10:32:38 +0530

    Updated and Corrected

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,18 +1,19 @@
 n = int(input("Enter the number of elements: "))
 list1 = []
-for i in range(0,n+1):
-    list1 = input() 
-    list1.append(list1)
+for i in range(0,n):
+    element = int(input()) 
+    list1.append(element)
+print(list1)
 
 key = int(input("Enter key element: "))
 
 for element in list1:
+    
     if key == element:
-        idx = element.index()
+        idx = list1.index(element)
         break
 
 print(idx)
-
 
 
 
